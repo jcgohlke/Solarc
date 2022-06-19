2c6f2c7f539f2b520a60f65d72a69e3da0761872
Ben Gohlke <jcgohlke@me.com>
2022-06-19 16:34:16 -0400

    Initial Commit

---
+++ Solarc/SolarcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarcApp.swift
+//  Solarc
+//
+//  Created by Joben Gohlke on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
